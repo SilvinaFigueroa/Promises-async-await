23d1c64dba066fea5a6f4061c7374a17bfd847a7
Silvina Figueroa <86253843+SilvinaFigueroa@users.noreply.github.com>
2024-05-29 22:34:00 -0700

    creating async function and userObj

--- index.mjs
+++ index.mjs
@@ -1,10 +1,44 @@
 // Importing database functions. DO NOT MODIFY THIS LINE.
-import { central, db1, db2, db3, vault } from "./databases.js";
+import { central, db1, db2, db3, vault } from "./database.mjs";
 
-function getUserData(id) {
-  const dbs = {
-    db1: db1,
-    db2: db2,
-    db3: db3
-  };
+let user = 10;
+
+getUserData(user)
+
+async function getUserData(user) {
+    let userObj = {}
+    const dbs = {
+        db1: db1,
+        db2: db2,
+        db3: db3
+    };
+
+    try {
+        const centralDbs = await central(user)
+        const vaultData = await vault(user)
+        const centralData = await dbs[centralDbs](user)
+
+        userObj = {
+            "id" : user,
+            "name" : vaultData.name,
+            "email" : vaultData.email,
+            "address" : vaultData.address,
+            "phone" : vaultData.phone,
+            "website" : centralData.website,
+            "company" : centralData.company,
+        }
+        
+        return console.log(userObj)
+
+    } catch (error) {
+        console.error(error)
+
+    }
+
+
 }
+
+
+
+
+
